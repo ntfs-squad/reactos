ddfc873f1209e83b5dd82badcda9e0bc9fec22d8
Justin Miller <Justin.miller@reactos.org>
2024-04-22 16:37:06 -0700

    [NTFS_NEW] Handle partition detection correctly

--- drivers/filesystems/ntfs_new/ntfs.h
+++ drivers/filesystems/ntfs_new/ntfs.h
@@ -12,7 +12,7 @@
 #include <pseh/pseh2.h>
 #include <section_attribs.h>
 
-#define TAG_NTFS '0ftN'
+#define TAG_NTFS 'NTFS'
 #define TAG_CCB 'CftN'
 #define TAG_FCB 'FftN'
 #define TAG_IRP_CTXT 'iftN'
@@ -60,8 +60,49 @@ FAST_IO_CHECK_IF_POSSIBLE NtfsFastIoCheckIfPossible;
 FAST_IO_READ NtfsFastIoRead;
 FAST_IO_WRITE NtfsFastIoWrite;
 
+
+#include <pshpack1.h>
+typedef struct _BIOS_PARAMETERS_BLOCK
+{
+    USHORT    BytesPerSector;			// 0x0B
+    UCHAR     SectorsPerCluster;		// 0x0D
+    UCHAR     Unused0[7];				// 0x0E, checked when volume is mounted
+    UCHAR     MediaId;				// 0x15
+    UCHAR     Unused1[2];				// 0x16
+    USHORT    SectorsPerTrack;		// 0x18
+    USHORT    Heads;					// 0x1A
+    UCHAR     Unused2[4];				// 0x1C
+    UCHAR     Unused3[4];				// 0x20, checked when volume is mounted
+} BIOS_PARAMETERS_BLOCK, *PBIOS_PARAMETERS_BLOCK;
+
+typedef struct _EXTENDED_BIOS_PARAMETERS_BLOCK
+{
+    USHORT    Unknown[2];				// 0x24, always 80 00 80 00
+    ULONGLONG SectorCount;			// 0x28
+    ULONGLONG MftLocation;			// 0x30
+    ULONGLONG MftMirrLocation;		// 0x38
+    CHAR      ClustersPerMftRecord;	// 0x40
+    UCHAR     Unused4[3];				// 0x41
+    CHAR      ClustersPerIndexRecord; // 0x44
+    UCHAR     Unused5[3];				// 0x45
+    ULONGLONG SerialNumber;			// 0x48
+    UCHAR     Checksum[4];			// 0x50
+} EXTENDED_BIOS_PARAMETERS_BLOCK, *PEXTENDED_BIOS_PARAMETERS_BLOCK;
+
+typedef struct _BOOT_SECTOR
+{
+    UCHAR     Jump[3];				// 0x00
+    UCHAR     OEMID[8];				// 0x03
+    BIOS_PARAMETERS_BLOCK BPB;
+    EXTENDED_BIOS_PARAMETERS_BLOCK EBPB;
+    UCHAR     BootStrap[426];			// 0x54
+    USHORT    EndSector;				// 0x1FE
+} BOOT_SECTOR, *PBOOT_SECTOR;
+#include <poppack.h>
+
 /* Utilities */
 void* __cdecl operator new(size_t Size, POOL_TYPE PoolType);
 
 #include "ntblockio.h"
 #include "ntfsdriver.h"
+#include "ntfspartition.h"
