616e03f8c4c6f16c61254ad4f5fb362bf0f285ad
Carl J. Bialorucki <carl.bialorucki@reactos.org>
2026-04-13 21:35:05 -0500

    Introduce ntfslib_new

---
+++ sdk/lib/fslib/ntfslib_new/util.cpp
@@ -0,0 +1,90 @@
+/*
+ * PROJECT:     ReactOS Kernel
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     NTFS filesystem driver
+ * COPYRIGHT:   Copyright 2024 Justin Miller <justin.miller@reactos.org>
+ *              Copyright 2024 Carl Bialorucki <carl.bialorucki@reactos.org>
+ */
+
+#include <ntifs.h>
+
+#ifndef TAG_NTFS
+#define TAG_NTFS 'NTFS'
+#endif // !TAG_NTFS
+
+ void* __cdecl operator new(size_t Size, POOL_TYPE PoolType)
+ {
+ 
+     Size = (Size != 0) ? Size : 1;
+     void* pObject = ExAllocatePoolWithTag(PoolType, Size, TAG_NTFS);
+ 
+ #if DBG
+     if (pObject != NULL)
+         RtlFillMemory(pObject, Size, 0xCD);
+ #endif // DBG
+ 
+     return pObject;
+ }
+ 
+ void* __cdecl operator new[](size_t Size, POOL_TYPE PoolType)
+ {
+ 
+     Size = (Size != 0) ? Size : 1;
+ 
+     void* pObject = ExAllocatePoolWithTag(PoolType, Size, TAG_NTFS);
+ 
+ #if DBG
+     if (pObject != NULL)
+         RtlFillMemory(pObject, Size, 0xCD);
+ #endif // DBG
+ 
+     return pObject;
+ }
+ 
+ void* __cdecl operator new(size_t Size, POOL_TYPE PoolType, ULONG Tag)
+ {
+ 
+     Size = (Size != 0) ? Size : 1;
+     void* pObject = ExAllocatePoolWithTag(PoolType, Size, Tag);
+ 
+ #if DBG
+     if (pObject != NULL)
+         RtlFillMemory(pObject, Size, 0xCD);
+ #endif // DBG
+ 
+     return pObject;
+ }
+ 
+ void* __cdecl operator new[](size_t Size, POOL_TYPE PoolType, ULONG Tag)
+ {
+ 
+     Size = (Size != 0) ? Size : 1;
+ 
+     void* pObject = ExAllocatePoolWithTag(PoolType, Size, Tag);
+ 
+ #if DBG
+     if (pObject != NULL)
+         RtlFillMemory(pObject, Size, 0xCD);
+ #endif // DBG
+ 
+     return pObject;
+ }
+ 
+ void __cdecl operator delete(void* pObject)
+ {
+     if (pObject != NULL)
+         ExFreePool(pObject);
+ }
+ 
+ void __cdecl operator delete(void* pObject, size_t s)
+ {
+     UNREFERENCED_PARAMETER( s );
+     ::operator delete( pObject );
+ }
+ 
+ void __cdecl operator delete[](void* pObject)
+ {
+     if (pObject != NULL)
+         ExFreePool(pObject);
+ }
+ 
