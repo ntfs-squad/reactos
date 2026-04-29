1a91e91b83b987d31b62c20bb0cc17d9dbb0da07
Carl J. Bialorucki <carl.bialorucki@reactos.org>
2026-04-29 15:18:36 -0500

    builds but has issues

--- sdk/lib/fslib/ntfslib_new/env/um/ntfs_um.h
+++ sdk/lib/fslib/ntfslib_new/env/um/ntfs_um.h
@@ -4,7 +4,14 @@ extern "C" {
 
 NTSTATUS
 NtfsDiskInitializeUm(
-    _In_ HANDLE FileHandle);
+    _In_      HANDLE FileHandle,
+    _Out_opt_ ULONG* BytesPerSector);
+
+/* HACK: Don't expose this */
+NTSTATUS
+NtfsReadVolume(_In_    ULONGLONG Offset,
+               _In_    ULONG Length,
+               _Inout_ PUCHAR Buffer);
 
 #ifdef __cplusplus
 }
